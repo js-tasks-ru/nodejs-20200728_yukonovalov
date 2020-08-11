c2b63324300713dbcf14916412ec88ceb8f0ff6c
yukonovalov <ykonovalov@outlook.com>
2020-08-11 19:27:22 +0300

    Add solution for streams

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,33 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.storage = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const string = this.storage + chunk.toString()
+    const index = string.indexOf(os.EOL);
+
+    if (index !== -1) {
+      this.push(string.slice(0, index))
+
+      this.storage = string.slice(index + 1);
+    } else {
+      this.storage = string;
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    this.storage
+      .split(os.EOL)
+      .forEach((line) => {
+        this.push(line);
+      });
+
+    callback();
   }
 }
 
